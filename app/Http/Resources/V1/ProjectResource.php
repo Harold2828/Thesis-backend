af6965a22de7ce989f48b97daaadb571180dbbcd
Harold2828 <harold2828@hotmail.com>
2024-04-06 03:29:05 -0500

    Creating Collections and Resources for Project and Project Details

---
+++ app/Http/Resources/V1/ProjectResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"=>$this->id,
+            "latitude"=>$this->latitude,
+            "longitude"=>$this->longitude,
+            "name"=>$this->name,
+            "description"=>$this->description,
+            "details"=>ProjectDetailResource::collection($this->whenLoaded("product_details"))
+
+        ];
+    }
+}
